4fe624c67c2209aab2276668a5ad682501b8813c
Matvey Kenya <matveykenya@gmail.com>
2021-12-08 06:39:52 +0500

    #0

---
+++ src/main/java/ru/netology/controller/PostController.java
@@ -0,0 +1,51 @@
+package ru.netology.controller;
+
+import com.google.gson.Gson;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+
+public class PostController {
+  public static final String APPLICATION_JSON = "application/json";
+  private final PostService service;
+  final Gson gson = new Gson();
+
+  public PostController(PostService service) {
+    this.service = service;
+  }
+
+  public void all(HttpServletResponse response) throws IOException {
+    final var data = service.all();
+    respPrint(response, data);
+  }
+
+  public void getById(long id, HttpServletResponse response) throws IOException{
+    try{
+      final var data = service.getById(id);
+      respPrint(response, data);
+    } catch (NotFoundException e) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+
+  public void save(Reader body, HttpServletResponse response) throws IOException {
+    final var post = gson.fromJson(body, Post.class);
+    final var data = service.save(post);
+    respPrint(response, data);
+  }
+
+  public void removeById(long id, HttpServletResponse response) {
+    if (!service.removeById(id)){
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+
+  private void respPrint(HttpServletResponse response, Object data) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    response.getWriter().print(gson.toJson(data));
+  }
+}
---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,40 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+// my realisation
+public class PostRepository {
+    private static volatile long countPosts = 0;
+    private static final Map<Long, Post> posts = new ConcurrentHashMap<>();
+
+    public List<Post> all() {
+        return posts.values().stream().toList();
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id)); // null если не нашли
+    }
+
+    public synchronized Post save(Post post) {
+        long id = post.getId();
+        if (id == 0){ // новая запись
+            countPosts++;
+            post.setId(countPosts);
+            posts.put(countPosts, post);
+        } else { // обновление существующей записи
+            if (posts.containsKey(id)) {
+                posts.put(id, post);
+            } else {
+                return null; // если не нашли старой записи
+            }
+        }
+        return post;
+    }
+
+    public boolean removeById(long id) {
+        return posts.remove(id) != null; // true если запись была такая
+    }
+}
